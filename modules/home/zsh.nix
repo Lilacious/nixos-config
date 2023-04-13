a68698ec87a21cd41ec724fa15deb849aa73b28f
lilacious <yuchenhe126@gmail.com>
2023-04-13 11:22:12 +0200

    improve update alias
    Signed-off-by: lilacious <yuchenhe126@gmail.com>

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -27,8 +27,7 @@
 
     shellAliases = {
       ll = "ls -l";
-      update-sisyphos = "cd ~/nixos-config && sudo nixos-rebuild switch --flake .#sisyphos";
-      update-hades = "cd ~/nixos-config && sudo nixos-rebuild switch --flake .#hade";
+      update = "cd ~/nixos-config && sudo nixos-rebuild switch --flake .#$HOST";
     };
   };
 }
