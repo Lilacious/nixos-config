506d3e6606d323c70565ab47ab37a2b5892397b9
lilacious <yuchenhe126@gmail.com>
2023-04-13 16:54:37 +0200

    fix
    Signed-off-by: lilacious <yuchenhe126@gmail.com>

--- hosts/sisyphos/configuration.nix
+++ hosts/sisyphos/configuration.nix
@@ -22,8 +22,8 @@
   services.openssh = {
   enable = true;
     # require public key authentication for better security
-    # passwordAuthentication = false;
-    # kbdInteractiveAuthentication = false;
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
     # permitRootLogin = "yes";
   };
 
