1aed03c5c121912735fe914f0610a7d0d02b92ad
lilacious <yuchenhe126@gmail.com>
2023-05-03 21:31:51 +0200

    remove kitty
    Signed-off-by: lilacious <yuchenhe126@gmail.com>

--- hosts/penelope/penelope-home.nix
+++ hosts/penelope/penelope-home.nix
@@ -7,7 +7,6 @@
     ../../modules/home/pkgs.nix
     ../../modules/home/alacritty.nix
     ../../modules/home/themes.nix
-    ../../modules/home/kitty.nix
     ../../modules/home/hyprland.nix
     ../../modules/home/eww.nix
 
