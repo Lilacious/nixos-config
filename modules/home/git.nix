26c5982b067088313e56674e2768166e51309938
Yuchen He <yuchenhe126@gmail.com>
2023-06-27 19:52:35 +0200

    add git aliases
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -5,5 +5,21 @@
     enable = true;
     userName = "Yuchen He";
     userEmail = "yuchenhe126@gmail.com";
+    aliases = {
+      ci = "commit -s";
+      co = "checkout";
+      br = "branch";
+      st = "status";
+      df = "diff";
+      dc = "diff --cached";
+      lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+      lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+      lolb = "log --graph --decorate --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all --simplify-by-decoration";
+      hist = "log --pretty=format:\"%h %ad|%s%d [%an]\" --graph --date=short";
+      pushcb = "push origin HEAD:refs/for/master";
+    };
+    ignores = [
+      "*.yunix"
+    ];
   };
 }
