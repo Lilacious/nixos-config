a00abd28bc0b8584b42b7d1557701e0b4ca7eca2
Yuchen He <yuchenhe126@gmail.com>
2023-09-26 05:43:09 +0200

    Add zsh login script to auto start Hyprland
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- hosts/penelope/home.nix
+++ hosts/penelope/home.nix
@@ -8,12 +8,22 @@
     ../../modules/home/alacritty.nix
     ../../modules/home/hyprland.nix
   ];
-  programs.zsh.shellAliases = {
-    open-front-door = "ssh door@bourbon.w17.io buzzer";
-    open-sesame = "ssh door@bourbon.w17.io open";
-    close-sesame = "ssh door@bourbon.w17.io close";
-    battery-info-internal = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
-    battery-info-external = "upower -i /org/freedesktop/UPower/devices/battery_BAT1";
+
+  programs.zsh = {
+    shellAliases = {
+      open-front-door = "ssh door@bourbon.w17.io buzzer";
+      open-sesame = "ssh door@bourbon.w17.io open";
+      close-sesame = "ssh door@bourbon.w17.io close";
+      battery-info-internal = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
+      battery-info-external = "upower -i /org/freedesktop/UPower/devices/battery_BAT1";
+    };
+
+    # Start Hyprland after login if standard user and current tty is tty1
+    loginExtra = ''
+      if [ $UID -ge 1000 ] && [ $(tty) = "/dev/tty1" ]; then
+        Hyprland
+      fi
+      '';
   };
   home.username = "yunix";
   home.homeDirectory = "/home/yunix";
