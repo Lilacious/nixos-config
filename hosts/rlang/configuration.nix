819e0812e0a49c812f100b5b56b658d21e05f24e
Yuchen He <yuchenhe126@gmail.com>
2023-10-21 18:39:22 +0200

    add curl
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- hosts/rlang/configuration.nix
+++ hosts/rlang/configuration.nix
@@ -67,8 +67,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim
     wget
+    curl
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
