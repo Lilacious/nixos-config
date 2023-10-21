8f9cd996a6642498c9965e3afacd561f4f0b3b06
Yuchen He <yuchenhe126@gmail.com>
2023-10-21 12:22:31 +0200

    fix
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- hosts/rlang/configuration.nix
+++ hosts/rlang/configuration.nix
@@ -20,7 +20,7 @@
   time.timeZone = "Europe/Berlin";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_DE.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "de";
