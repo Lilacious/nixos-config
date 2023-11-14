e467c485e4d5a8f2b6264245a5d4c975fe7c17f2
Yuchen He <yuchenhe126@gmail.com>
2023-11-14 04:06:22 +0100

    Add catppuccin theme for fcitx5
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- hosts/hades/modules/cloud.nix
+++ hosts/hades/modules/cloud.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 let
   ## Transcoding
   go-vod = pkgs.callPackage ../../../pkgs/go-vod.nix {};
--- modules/programs/input-method.nix
+++ modules/programs/input-method.nix
@@ -1,18 +1,26 @@
 { config, pkgs, lib, ... }:
 
 with lib;
+let 
+  fcitx5-catppuccin = pkgs.callPackage ../../pkgs/fcitx5-catppuccin/package.nix {};
+in
 {
   i18n.inputMethod = mkIf config.myModules.desktop.enable (mkMerge [
     ({
       enabled = "fcitx5";
       fcitx5.addons = with pkgs; [
         fcitx5-mozc
+        fcitx5-gtk
       ];
     })
     (mkIf config.myModules.desktop.plasma5.enable {
       fcitx5.addons = with pkgs; [
         libsForQt5.fcitx5-qt
+        fcitx5-catppuccin
       ];
     })
   ]);
+
+  environment.systemPackages = [
+  ];
 }
