777f071ed1fed672d5bf7066bd2b3e2dfb6a0502
Yuchen He <yuchenhe126@gmail.com>
2023-12-05 15:55:29 +0100

    Update flake and remove LC_ALL
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -25,7 +25,7 @@ with lib;
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ALL = "en_US.UTF-8";
+    ## LC_ALL = "C";
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
     LC_MEASUREMENT = "de_DE.UTF-8";
