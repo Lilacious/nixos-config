79448cf539e20390d49422e31f07fe8cd445722e
Yuchen He <yuchenhe126@gmail.com>
2023-12-16 18:33:38 +0100

    Change LC_ALL to en_IE
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -14,6 +14,7 @@ with lib;
   ];
 
   i18n.supportedLocales = [
+    "en_IE.UTF-8/UTF-8"
     "en_US.UTF-8/UTF-8"
     "de_DE.UTF-8/UTF-8"
     "ja_JP.UTF-8/UTF-8"
@@ -25,17 +26,16 @@ with lib;
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    ## LC_ALL = "C";
-    LANGUAGE = "en_US.UTF-8";
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
+    LC_ALL = "en_IE.UTF-8";
+    #LC_ADDRESS = "de_DE.UTF-8";
+    #LC_IDENTIFICATION = "de_DE.UTF-8";
+    #LC_MEASUREMENT = "de_DE.UTF-8";
+    #LC_MONETARY = "de_DE.UTF-8";
+    #LC_NAME = "de_DE.UTF-8";
+    #LC_NUMERIC = "de_DE.UTF-8";
+    #LC_PAPER = "de_DE.UTF-8";
+    #LC_TELEPHONE = "de_DE.UTF-8";
+    #LC_TIME = "de_DE.UTF-8";
   };
 
   ## Default keymap
