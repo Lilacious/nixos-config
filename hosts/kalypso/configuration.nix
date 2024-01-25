da0a52233ca3c97aa7cc402a63c96ef3a68762f9
Yuchen He <yuchenhe126@gmail.com>
2024-01-25 03:46:07 +0100

    configure kalypso
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

---
+++ hosts/kalypso/configuration.nix
@@ -0,0 +1,52 @@
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./packages.nix
+    ../../users/user.nix
+    ../../modules
+  ];
+  system.stateVersion = "23.11";
+
+  networking.hostName = "kalypso";
+
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi"; 
+    };
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+    };
+  };
+
+  myModules = {
+    ## Enable desktop environment
+    desktop = {
+      enable = true;
+      plasma5.enable = true;
+    };
+
+    services = {
+      ## Enable printing
+      printing.enable = false;
+
+      ## Enable virtualization
+      virtualization.enable = true;
+
+      syncthing.enable = true;
+    };
+
+    programs = {
+      cysecTools.enable = false;
+      firefoxDevedition.enable = false;
+    };
+  };
+
+  ## Enable networking
+  networking.networkmanager.enable = true;
+
+  ## Bluetooth
+  hardware.bluetooth.enable = true;
+}
