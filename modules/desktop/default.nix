893ee9a2846572920584ad9e4b11a82761f48ef5
Yuchen He <yuchenhe126@gmail.com>
2024-01-28 16:28:09 +0100

    Add pipewire options
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -23,9 +23,8 @@ with lib;
     };
   
     # Enable sound with pipewire.
-    sound.enable = true;
+    sound.enable = false;
     hardware.pulseaudio.enable = false;
-    security.polkit.enable = true;
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
@@ -37,8 +36,20 @@ with lib;
       # use the example session manager (no others are packaged yet so this is enabled by default,
       # no need to redefine it in your config for now)
       #media-session.enable = true;
+      # Audio crackling fix
+      #extraConfig.pipewire = {
+      #  "context.properties" = [{
+      #    "link.max-buffers" = 128;
+      #    "default.clock.allowed-rates" = [ 44100 48000 96000 ];
+      #    "default.clock.rate" = 48000;
+      #    "default.clock.quantum" = 512;
+      #    "default.clock.min-quantum" = 32;
+      #    "default.clock.max-quantum" = 8192;
+      #  }];
+      #};
     };
   
+    security.polkit.enable = true;
     ## GNOME programs
     programs.dconf.enable = true;
 
