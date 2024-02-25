cbd4c20bb22faa069ca88a3ad8ce91e447bbe235
Yuchen He <yuchenhe126@gmail.com>
2024-02-25 03:09:11 +0100

    add reverse proxy service
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./printing.nix
+    ./reverse-proxy.nix
     ./syncthing.nix
     ./virtualization.nix
   ];
---
+++ modules/services/reverse-proxy.nix
@@ -0,0 +1,33 @@
+{ config, lib, variables, ... }:
+
+with lib;
+let
+  cfg = config.myModules.services.reverseProxy;
+in
+{
+  options = {
+    myModules.services.reverseProxy = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+      };
+    };
+  };
+  config = mkIf (cfg.enable) {
+    security.acme.acceptTerms = true;
+    security.acme.defaults.email = "yuchenhe126@gmail.com";
+
+    services.nginx = {
+      enable = true;
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+    };
+
+    networking.firewall.allowedTCPPorts = [
+      80
+      443
+    ];
+  };
+}
