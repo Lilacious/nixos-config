9a27e0977d12936519551e23ca62eef7e29d3f7b
Yuchen He <yuchenhe126@gmail.com>
2024-02-26 14:04:05 +0100

    disable other modules
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- hosts/hades/modules/default.nix
+++ hosts/hades/modules/default.nix
@@ -1,9 +1,9 @@
 {
   imports = [
     ./cloud.nix
-    ./dashboard.nix
-    ./syncthing.nix
-    ./torrent.nix
+    #./dashboard.nix
+    #./syncthing.nix
+    #./torrent.nix
   ];
 
   myModules.services.reverseProxy.enable = true;
