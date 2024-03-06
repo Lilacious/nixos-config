acc89f426d595cc1da57a4f36beb39fac2bdd6ca
Yuchen He <yuchenhe126@gmail.com>
2024-03-06 19:17:17 +0100

    change looking glass behaviour
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- hosts/kalypso/vfio.nix
+++ hosts/kalypso/vfio.nix
@@ -5,18 +5,45 @@ with lib;
   boot.kernelParams = [ "intel_iommu=on" ];
   boot.kernelModules = [ "vfio" "vfio_iommu_type1" "vfio_pci" "vfio_virqfd" ];
 
+  ## Isolate guest GPU
   boot.extraModprobeConfig = ''
     options vfio-pci ids=10de:2782,10de:22bc
   '';
-  
+
+  ## Looking Glass
+  home-manager.users.${variables.username} = {
+    programs.looking-glass-client = {
+      enable = true;
+      settings = {
+        win = {
+          fullScreen = true;
+          #size = "2560x1440";
+          size = "1920x1080";
+        };
+      };
+    };
+
+    ## Override Looking Glass desktop entry
+    xdg.desktopEntries = {
+      looking-glass-client = {
+        exec = "looking-glass-client";
+        icon = "lg-logo";
+        name = "Looking Glass Client";
+        terminal = false;
+        type = "Application";
+      };
+    };
+  };
+
+  ## Looking Glass shared mem permission
   systemd.tmpfiles.rules = [
     "f /dev/shm/looking-glass 0660 ${variables.username} kvm -"
   ];
+
   myModules.services.virtualization.enable = mkForce true;
 
   environment.systemPackages = with pkgs; [
     libguestfs-with-appliance
-    looking-glass-client
   ];
 
   virtualisation = {
