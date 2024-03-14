0e61024b3d0df4ad27c731f4a9ca96f9aa9016b5
Yuchen He <yuchenhe126@gmail.com>
2024-03-14 11:53:11 +0100

    Disable automatic garbage collection
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,17 +1,9 @@
-{ lib, ... }:
-
-with lib;
 {
   nix = {
     settings = {
       experimental-features = "nix-command flakes";
       auto-optimise-store = true;
       warn-dirty = false;
-    };
-    gc = {
-      automatic = mkDefault true;
-      dates = "weekly";
-      options = "--delete-older-than +5";
     };
   };
 }
