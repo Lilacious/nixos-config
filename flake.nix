b7c9198174a75378db195ecf10119fd3478b2061
Yuchen He <yuchenhe126@gmail.com>
2024-03-14 07:07:29 +0100

    init flake-parts
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,12 +1,36 @@
 {
   description = "My really basic flaky flake";
 
+  outputs = { 
+    self, nixpkgs, 
+    home-manager, flake-parts, nixos-hardware, agenix, 
+    plasma-manager, 
+    ... 
+  }@inputs:
+  flake-parts.lib.mkFlake { inherit inputs; }
+  {
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+    imports = [
+    ];
+
+    flake = {
+      nixosConfigurations = (
+        import ./hosts { 
+          inherit (nixpkgs) lib;
+          inherit inputs nixpkgs;
+        }
+      );
+    };
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     agenix = {
       url = "github:ryantm/agenix";
@@ -17,20 +41,5 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "nixpkgs";
     };
-  };
-
-  outputs = { 
-    self, nixpkgs, 
-    home-manager, nixos-hardware, agenix, 
-    plasma-manager, 
-    ... 
-  }@inputs:
-  {
-    nixosConfigurations = (
-      import ./hosts { 
-        inherit (nixpkgs) lib;
-        inherit inputs nixpkgs;
-      }
-    );
   };
 }
