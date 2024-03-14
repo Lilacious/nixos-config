ef59942028dca7bdec81ac8c62866753d067319b
Yuchen He <yuchenhe126@gmail.com>
2024-03-14 23:57:56 +0100

    modules: Inplements myModules option
    The myModules option disables local modules if disabled.
    
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/default.nix
+++ modules/default.nix
@@ -1,4 +1,13 @@
-{
+{lib, ...}:
+with lib; {
+  options.myModules = mkOption {
+    type = types.bool;
+    default = true;
+    description = lib.mdDoc ''
+      Disables local modules entirely if disabled.
+    '';
+  };
+
   imports = [
     ./core
   ];
