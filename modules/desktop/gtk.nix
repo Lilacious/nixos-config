0a383ef9e9c87570eba7d796f47aca058cbde162
Yuchen He <yuchenhe126@gmail.com>
2024-03-14 22:25:45 +0100

    conditional implementation
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/desktop/gtk.nix
+++ modules/desktop/gtk.nix
@@ -1,25 +1,29 @@
-{ pkgs, config, ... }:
+{ lib, pkgs, config, ... }:
+with lib;
 let
   homecfg = config.home-manager.users.${config.variables.username};
+  cfg = config.myModules.desktop;
 in
 {
-  home-manager.users.${config.variables.username} = {
-    home.pointerCursor = {
-      package = pkgs.catppuccin-cursors.mochaPink;
-      name = "Catppuccin-Mocha-Pink-Cursors";
-      size = 16;
-      gtk.enable = true;
-    };
+  config = mkIf cfg.enable {
+    home-manager.users.${config.variables.username} = {
+      home.pointerCursor = {
+        package = pkgs.catppuccin-cursors.mochaPink;
+        name = "Catppuccin-Mocha-Pink-Cursors";
+        size = 16;
+        gtk.enable = true;
+      };
 
-    gtk = {
-      enable = true;
-      gtk2.configLocation = "${homecfg.xdg.configHome}/gtk-2.0/gtkrc";
-      theme = {
-        name = "Catppuccin-Mocha-Compact-Pink-Dark";
-        package = pkgs.catppuccin-gtk.override {
-          accents = [ "pink" ];
-          size = "compact";
-          variant = "mocha";
+      gtk = {
+        enable = true;
+        gtk2.configLocation = "${homecfg.xdg.configHome}/gtk-2.0/gtkrc";
+        theme = {
+          name = "Catppuccin-Mocha-Compact-Pink-Dark";
+          package = pkgs.catppuccin-gtk.override {
+            accents = [ "pink" ];
+            size = "compact";
+            variant = "mocha";
+          };
         };
       };
     };
