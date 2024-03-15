094ed14473fbe9d06b3c301c8b85c1cc4397cea3
Yuchen He <yuchenhe126@gmail.com>
2024-03-15 01:59:56 +0100

    common.nix: Add firewall rule
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/core/common.nix
+++ modules/core/common.nix
@@ -7,9 +7,9 @@ with lib; let
   cfg = config.myModules;
 in {
   config = mkIf cfg {
-    ## Default time zone
+    networking.firewall.enable = mkForce true;
     time.timeZone = mkDefault "Europe/Berlin";
-    programs.dconf.enable = mkDefault true;
+    #programs.dconf.enable = mkDefault true;
     hardware.enableRedistributableFirmware = mkDefault true;
     nixpkgs.config.allowUnfree = mkDefault true;
   };
