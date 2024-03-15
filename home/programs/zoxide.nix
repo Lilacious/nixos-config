266622ea7bb92dd8e31993c1f6c7ba69389b56fd
Yuchen He <yuchenhe126@gmail.com>
2024-03-15 18:54:57 +0100

    zoxide.nix: Init implement an option to enable the program
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- home/programs/zoxide.nix
+++ home/programs/zoxide.nix
@@ -1,11 +1,28 @@
-{config, ...}: {
-  programs = {
-    zoxide = {
-      enable = true;
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.myHome.programs.eza;
+in {
+  options = {
+    myHome.programs.eza = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+      };
     };
   };
-  shellAliases = {
-    cd = "z"; # zoxide
-    cdf = "zi"; # z with fzf
+  config = mkIf cfg.enable {
+    programs = {
+      zoxide = {
+        enable = true;
+      };
+    };
+    shellAliases = {
+      cd = "z"; # zoxide
+      cdf = "zi"; # z with fzf
+    };
   };
 }
