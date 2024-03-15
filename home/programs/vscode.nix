a379563930f8d4eb4d091dc77673c6e2e665b3ee
Yuchen He <yuchenhe126@gmail.com>
2024-03-15 17:54:07 +0100

    vscode.nix: Init vscode in home
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

---
+++ home/programs/vscode.nix
@@ -0,0 +1,68 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.myModules.programs.vscode;
+  jsonFormat = pkgs.formats.json {};
+in {
+  options = {
+    myModules.programs.vscode = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+      };
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.vscodium;
+      };
+
+      userSettings = mkOption {
+        type = jsonFormat.type;
+        default = {};
+      };
+
+      extensions = mkOption {
+        type = types.listOf types.package;
+        default = [];
+      };
+    };
+  };
+  config = mkIf (cfg.enable) {
+    programs.vscode = {
+      enable = true;
+
+      package = cfg.package;
+
+      enableExtensionUpdateCheck = false;
+
+      enableUpdateCheck = false;
+
+      userSettings =
+        {
+          "nix"."enableLanguageServer" = true;
+          "nix"."serverPath" = "nil";
+          "vim"."useSystemClipboard" = true;
+          "vim"."enableNeovim" = true;
+          "vim"."neovimUseConfigFile" = true;
+          "extensions"."experimental"."affinity" = {
+            "vscodevim"."vim" = 1;
+          };
+          "workbench"."colorTheme" = "Catppuccin Mocha";
+          "catppuccin"."accentColor" = "pink";
+        }
+        // cfg.userSettings;
+
+      extensions = with pkgs.vscode-extensions;
+        [
+          jnoortheen.nix-ide
+          vscodevim.vim
+          catppuccin.catppuccin-vsc
+        ]
+        ++ cfg.extensions;
+    };
+  };
+}
