67c1692013af15357f539d8bac6a86ccb3ea3d89
Yuchen He <yuchenhe126@gmail.com>
2024-03-16 20:36:28 +0100

    theme.nix: Init theme in other
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

---
+++ home/other/theme.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.myHome.other.theme;
+  dsktp = config.myModules.desktop;
+in {
+  options = {
+    myHome.other.theme = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+      };
+    };
+  };
+
+  config = mkIf (cfg.enable && dsktp.enable) {
+    home.pointerCursor = {
+      package = pkgs.catppuccin-cursors.mochaPink;
+      name = "Catppuccin-Mocha-Pink-Cursors";
+      size = 16;
+      gtk.enable = true;
+    };
+
+    gtk = {
+      enable = true;
+      gtk2.configLocation = "${xdg.configHome}/gtk-2.0/gtkrc";
+      theme = {
+        name = "Catppuccin-Mocha-Compact-Pink-Dark";
+        package = pkgs.catppuccin-gtk.override {
+          accents = ["pink"];
+          size = "compact";
+          variant = "mocha";
+        };
+      };
+    };
+  };
+}
