00b9256ced042d805a0737d43653316a276b4a15
Yuchen He <yuchenhe126@gmail.com>
2024-03-16 04:58:15 +0100

    users/yunix: Reimplement the structure of the user folder
    Completely remove conditionals from yunix.
    Move the content from yunix.nix to default.nix and also
    remove yunix.nix.
    
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- users/yunix/default.nix
+++ users/yunix/default.nix
@@ -1,6 +1,24 @@
-{
-  imports = [
-    ./yunix.nix
-    ./git.nix
-  ];
+{config, ...}: {
+  users.users.yunix = {
+    isNormalUser = true;
+    description = "Yunix";
+    extraGroups = [
+      "libvirtd"
+      "networkmanager"
+      "video"
+      "wheel"
+    ];
+    initialPassword = "changeme";
+    shell = "zsh";
+  };
+
+  home-manager.users.yunix = {
+    home.username = "yunix";
+    home.homeDirectory = "${config.users.users.yunix.home}";
+    home.stateVersion = "23.05";
+    imports = [
+      ./git.nix
+      ../../home
+    ];
+  };
 }
