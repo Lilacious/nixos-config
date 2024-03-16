1eee732b53e002eabf508ea30712df20f51dc336
Yuchen He <yuchenhe126@gmail.com>
2024-03-16 20:21:15 +0100

    secrets: Init secrets folder
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

---
+++ secrets/default.nix
@@ -0,0 +1,13 @@
+{
+  inputs,
+  system,
+  ...
+}: {
+  imports = [
+    inputs.agenix.nixosModules.default
+  ];
+
+  environment.systemPackages = [
+    inputs.agenix.packages.${system}.default
+  ];
+}
---
+++ secrets/secrets.nix
@@ -0,0 +1,11 @@
+let
+  nas-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzOvjEuo5/HAVphmO20zd7ltt0nSY9L4hsT/2HWjqWw";
+  laptop-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGc5xk4R+2CfNV1fyyvZEZgzjaRomO0hCJ55E00pSOsv";
+  users = [nas-user laptop-user];
+
+  nas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLN5Wu+GSsdLJfPZXy8q6CGL9VmCHN2hTObNpbXf5YR";
+  laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHGTfIF2MLDp01ISXoP67lymVV7cnQugW5SP/E5zfgkk";
+  systems = [nas laptop];
+in {
+  "nextcloud1.age".publicKeys = users ++ systems;
+}
