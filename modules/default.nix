95b2663449def5778d1987c797b896cfdceb0b29
Yuchen He <yuchenhe126@gmail.com>
2024-03-17 00:18:36 +0100

    modules: Change modules option
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/default.nix
+++ modules/default.nix
@@ -1,11 +1,13 @@
 {lib, ...}:
 with lib; {
-  options.myModules = mkOption {
-    type = types.bool;
-    default = true;
-    description = lib.mdDoc ''
-      Disables local modules entirely if disabled.
-    '';
+  options.myModules = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = lib.mdDoc ''
+        Disables local modules entirely if disabled.
+      '';
+    };
   };
 
   imports = [
