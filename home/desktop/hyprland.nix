58c00b0d89c62d9ce47302676317f2aaf6363adf
Yuchen He <yuchenhe126@gmail.com>
2024-03-17 17:38:58 +0100

    hyprland.nix: Set env vars in hyprland config
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -65,22 +65,20 @@ in {
       xdg-desktop-portal-gtk
     ];
 
-    home.sessionVariables = {
-      GDK_BACKEND = "wayland,x11";
-      QT_QPA_PLATFORM = "wayland;xcb";
-      #QT_AUTO_SCREEN_SCALE_FACTOR = 1;
-      #QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
-      SDL_VIDEODRIVER = "wayland";
-      CLUTTER_BACKEND = "wayland";
-      XDG_SESSION_TYPE = "wayland";
-      XDG_SESSION_DESKTOP = "Hyprland";
-    };
-
     wayland.windowManager.hyprland = {
       enable = true;
-
       settings =
         {
+          env = [
+            "GDK_BACKEND,wayland,x11"
+            "QT_QPA_PLATFORM,wayland;xcb"
+            "SDL_VIDEODRIVER,wayland"
+            "CLUTTER_BACKEND,wayland"
+            "XDG_CURRENT_DESKTOP,Hyprland"
+            "XDG_SESSION_TYPE,wayland"
+            "XDG_SESSION_DESKTOP,Hyprland"
+          ];
+
           general = {
             border_size = 2;
             ## Gaps between windows
