8390066db303a887c7d52960f7c48db9c6125e21
Yuchen He <yuchenhe126@gmail.com>
2024-03-17 01:05:18 +0100

    hyprland.nix: Disable hyprland by default
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -11,7 +11,7 @@ in {
     myHome.desktop.hyprland = {
       enable = mkOption {
         type = types.bool;
-        default = true;
+        default = false;
       };
 
       terminal = mkOption {
