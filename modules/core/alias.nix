de3051c23ad301c05579d671d26a7830984a3b42
Yuchen He <yuchenhe126@gmail.com>
2024-12-21 17:29:12 +0100

    core/alias.nix: set update alias for darwin hosts
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/core/alias.nix
+++ modules/core/alias.nix
@@ -13,5 +13,8 @@ with lib;
     (mkIf pkgs.stdenv.isLinux {
       update = "cd ~/nixos-config && sudo nixos-rebuild switch --flake .#$HOST";
     })
+    (mkIf pkgs.stdenv.isDarwin {
+      update = "cd ~/nixos-config && darwin-rebuild switch --flake .#$HOST";
+    })
   ];
 }
