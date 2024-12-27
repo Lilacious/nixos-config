17dcf530d8791ba2ad37f3e500bed8ad1cec4abe
Yuchen He <yuchenhe126@gmail.com>
2024-12-27 01:59:33 +0100

    profiles: add desktop profile
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- profiles/default.nix
+++ profiles/default.nix
@@ -1,6 +1,7 @@
 {
   flake = {
     nixosModules = {
+      desktop = import ./desktop.nix;
       nixos = import ./nixos.nix;
       server = import ./server.nix;
       workstation = import ./workstation.nix;
--- profiles/workstation.nix
+++ profiles/workstation.nix
@@ -1,43 +1,12 @@
 {
-  inputs,
-  config,
   lib,
   pkgs,
   ...
 }:
 with lib;
-let
-  username = "yunix";
-  gitFile = ../users/${username}/git.nix;
-in
 {
   imports = [
-    ./nixos.nix
-    ../modules
-
-    inputs.home-manager.nixosModules.home-manager
-    {
-      home-manager = {
-        extraSpecialArgs = {
-          inherit inputs;
-        };
-        useGlobalPkgs = true;
-        useUserPackages = true;
-
-        users.${username} = {
-          home = {
-            username = "${username}";
-            homeDirectory = "${config.users.users.${username}.home}";
-            stateVersion = "23.05";
-          };
-          imports = [
-            ../home
-          ] ++ optional (builtins.pathExists gitFile) gitFile;
-        };
-      };
-    }
-
-    ../users/${username}
+    ./desktop.nix
   ];
 
   environment.systemPackages = with pkgs; [
