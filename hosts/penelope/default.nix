4088a9d1aa1ef17aa371c80a2c2d285071edbc84
Yuchen He <yuchenhe126@gmail.com>
2025-01-27 12:45:26 +0100

    hosts/penelope: update alias domain name
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- hosts/penelope/default.nix
+++ hosts/penelope/default.nix
@@ -20,9 +20,9 @@ in
         {
           ## Custom aliases for penelope
           home-manager.users.yunix.home.shellAliases = {
-            open-front-door = "ssh door@bourbon.w17.io buzzer";
-            open-sesame = "ssh door@bourbon.w17.io open";
-            close-sesame = "ssh door@bourbon.w17.io close";
+            open-front-door = "ssh door@burbon.cccda.de buzzer";
+            open-sesame = "ssh door@burbon.cccda.de open";
+            close-sesame = "ssh door@burbon.cccda.de close";
             battery-info-internal = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
             battery-info-external = "upower -i /org/freedesktop/UPower/devices/battery_BAT1";
           };
