38dee6b871d6a8e79f25fbfb64f24cbeb40dbc4c
Yuchen He <yuchenhe126@gmail.com>
2025-02-18 02:41:00 +0100

    hosts/penelope: add MacOS grub entry
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- hosts/penelope/configuration.nix
+++ hosts/penelope/configuration.nix
@@ -12,6 +12,17 @@
       enable = true;
       efiSupport = true;
       device = "nodev";
+
+      extraEntries = ''
+                menuentry 'MacOS' --class macosx {
+        	        insmod chain
+        	        insmod part_gpt
+                  insmod fat
+                  insmod search_fs_uuid
+                  search --fs-uuid --no-floppy --set=root 4696-767D
+        	        chainloader ($root)/efi/MacOS-opencore/oc/OpenCore.efi
+                }
+      '';
     };
   };
   ## Power/Battery
