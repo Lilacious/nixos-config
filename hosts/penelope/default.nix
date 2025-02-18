fb2b1a76369c283fd2b097af1ea530d33ddad593
Yuchen He <yuchenhe126@gmail.com>
2025-02-18 02:53:47 +0100

    hosts/penelope: add darwinConfiguration
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- hosts/penelope/default.nix
+++ hosts/penelope/default.nix
@@ -4,30 +4,50 @@ let
   nixhw = inputs.nixos-hardware.nixosModules;
 in
 {
-  flake.nixosConfigurations = {
-    penelope = lib.nixosSystem {
-      specialArgs = {
-        inherit self inputs;
-        system = "x86_64-linux";
-      };
-      modules = [
-        nixhw.lenovo-thinkpad-t480
-        ./hardware-configuration.nix
-        ./configuration.nix
-        ./modules
-        self.nixosModules.workstation
+  flake = {
+    nixosConfigurations = {
+      penelope = lib.nixosSystem {
+        specialArgs = {
+          inherit self inputs;
+          system = "x86_64-linux";
+        };
+        modules = [
+          nixhw.lenovo-thinkpad-t480
+          ./hardware-configuration.nix
+          ./configuration.nix
+          ./modules
+          self.nixosModules.workstation
 
-        {
-          ## Custom aliases for penelope
-          home-manager.users.yunix.home.shellAliases = {
-            open-front-door = "ssh door@burbon.cccda.de buzzer";
-            open-sesame = "ssh door@burbon.cccda.de open";
-            close-sesame = "ssh door@burbon.cccda.de close";
-            battery-info-internal = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
-            battery-info-external = "upower -i /org/freedesktop/UPower/devices/battery_BAT1";
-          };
-        }
-      ];
+          {
+            ## Custom aliases for penelope
+            home-manager.users.yunix.home.shellAliases = {
+              open-front-door = "ssh door@burbon.cccda.de buzzer";
+              open-sesame = "ssh door@burbon.cccda.de open";
+              close-sesame = "ssh door@burbon.cccda.de close";
+              battery-info-internal = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
+              battery-info-external = "upower -i /org/freedesktop/UPower/devices/battery_BAT1";
+            };
+          }
+        ];
+      };
+    };
+    darwinConfigurations = {
+      penelope-darwin = inputs.darwin.lib.darwinSystem {
+        specialArgs = {
+          inherit self inputs;
+          system = "x86_64-darwin";
+        };
+
+        modules = [
+          ./darwin-configuration.nix
+          self.darwinModules.darwin
+
+          {
+            networking.hostName = "penelope-darwin";
+            nixpkgs.hostPlatform = "x86_64-darwin";
+          }
+        ];
+      };
     };
   };
 }
