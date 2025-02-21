618fa884a7f42ca839213ed711512644358e0572
Yuchen He <yuchenhe126@gmail.com>
2025-02-21 10:47:45 +0100

    hosts/penelope: fix formating and add fwupd commented out
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- hosts/penelope/configuration.nix
+++ hosts/penelope/configuration.nix
@@ -14,17 +14,20 @@
       device = "nodev";
 
       extraEntries = ''
-                menuentry 'MacOS' --class macosx {
-        	        insmod chain
-        	        insmod part_gpt
-                  insmod fat
-                  insmod search_fs_uuid
-                  search --fs-uuid --no-floppy --set=root 4696-767D
-        	        chainloader ($root)/efi/MacOS-opencore/oc/OpenCore.efi
-                }
+        menuentry 'MacOS' --class macosx {
+          insmod chain
+          insmod part_gpt
+          insmod fat
+          insmod search_fs_uuid
+          search --fs-uuid --no-floppy --set=root 4696-767D
+          chainloader ($root)/efi/MacOS-opencore/oc/OpenCore.efi
+        }
       '';
     };
   };
+  ## Update firmware
+  #services.fwupd.enable = true;
+
   ## Power/Battery
   powerManagement.powertop.enable = true;
   ## Enable networking
