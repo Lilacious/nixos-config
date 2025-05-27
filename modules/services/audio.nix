eb843708e7927f3d24a64a871cf3a8fad1fc367e
Yuchen He <yuchenhe126@gmail.com>
2025-05-27 18:37:08 +0200

    services/audio.nix: fix audio module
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/services/audio.nix
+++ modules/services/audio.nix
@@ -14,11 +14,16 @@ in
     };
   };
   config = mkIf cfg.enable {
-    ## Printer with avahi
-    services.printing.enable = true;
-    services.avahi.enable = true;
-    services.avahi.nssmdns4 = true;
-    ## For a WiFi printer
-    services.avahi.openFirewall = mkDefault true;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+    };
   };
 }
