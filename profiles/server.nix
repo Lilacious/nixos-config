273abf96bef24f4bc9bea74ffd01c1bc016bcc90
Yuchen He <yuchenhe126@gmail.com>
2025-06-15 15:32:11 +0200

    profiles/server: disable html and info docs
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- profiles/server.nix
+++ profiles/server.nix
@@ -12,6 +12,12 @@
       systemctl --no-pager --failed || true
   '';
 
+  documentation = {
+    # html docs and info are not required, man pages are enough
+    doc.enable = false;
+    info.enable = false;
+  };
+
   services.openssh = {
     enable = true;
     settings = {
