cd7d176ce58eb7cb7da8f4d840523155716b538c
Yuchen He <yuchenhe126@gmail.com>
2025-10-10 21:34:28 +0200

    core/zsh.nix: fix broken historySubstringSearch
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- home/core/zsh.nix
+++ home/core/zsh.nix
@@ -1,6 +1,7 @@
 # For system config see nix-config/modules/core/zsh.nix
 {
   config,
+  pkgs,
   lib,
   ...
 }:
@@ -31,11 +32,28 @@ in
         expireDuplicatesFirst = true;
       };
 
+      # historySubstringSearch broken by
+      # https://github.com/nix-community/home-manager/commit/04f672b5db5ed5b2fa56aff523089cd29049cb4d
       historySubstringSearch = {
-        enable = true;
+        # disable current config
+        # enable = true;
         searchUpKey = "$terminfo[kcuu1]";
         searchDownKey = "$terminfo[kcud1]";
       };
+      # use old config
+      initContent =
+        let
+          cfg = config.programs.zsh;
+        in
+        lib.mkOrder 1250 ''
+          source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+          ${lib.concatMapStringsSep "\n" (upKey: ''bindkey "${upKey}" history-substring-search-up'') (
+            lib.toList cfg.historySubstringSearch.searchUpKey
+          )}
+          ${lib.concatMapStringsSep "\n" (downKey: ''bindkey "${downKey}" history-substring-search-down'') (
+            lib.toList cfg.historySubstringSearch.searchDownKey
+          )}
+        '';
     };
   };
 }
